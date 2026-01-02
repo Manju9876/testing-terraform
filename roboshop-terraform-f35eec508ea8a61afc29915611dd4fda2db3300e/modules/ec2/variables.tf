9cc2ebabc03fbdbaccc1d92e2e8d04a8fd2c4e82
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2026-01-02 21:06:42 +0530

    teraform code

---
+++ roboshop-terraform-f35eec508ea8a61afc29915611dd4fda2db3300e/modules/ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {}
+variable "name" {}
+variable "instance_type" {}
+variable "vpc_security_group_ids" {}
+variable "zone_id" {}
+variable "env" {}
+variable "vault_token" {}
+variable "ansible_role" {}
+variable "root_volume_size" {}
